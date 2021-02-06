9b5994a2c2da5e1f013918b6207973e3054c1ec2
YounglanHong <yong2401@gmail.com>
2021-02-06 15:55:53 +0900

    🎉 Start a next.js devlog

---
+++ hylog/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+// import Image from 'next/image'
+// import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return (
+    <>
+    <Head>
+      <title>Hylog</title>
+    </Head>
+    <h1>Hylog</h1>
+    </>
+  )
+}
+
+export default Home
